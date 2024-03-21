e9a72285c365f3742502a946ae228af146dc9671
eliuttth-dev <eliuttth@gmail.com>
2024-03-21 11:17:45 -0300

    Add login controller

--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -1,1 +1,18 @@
-// Login Controller
+import { Request, Response } from "express";
+import { loginUser, searchUser } from "../models/user.model";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export default async function loginController(req: Request, res: Response) {
+  const { email, username, password } = req.body;
+
+  try {
+    const validUserToken = await loginUser(email, username, password);
+
+    res.set("Authorization", `Bearer ${validUserToken}`);
+
+    res.status(200).json({ status: "success", message: "User logged correctly" });
+  } catch (error) {
+    console.error("Something went wrong", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
